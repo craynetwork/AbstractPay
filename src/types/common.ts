0dea0309d8cf5a073742c7e0ad957e0421a12fc6
ptsayli <ptsayli@gmail.com>
2025-04-16 17:56:04 +0400

    update payload type and bump version to 0.1.5

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,6 +1,10 @@
 
 export interface IDestinationAction {
-  payload: string // The bytecode for the action to execute on the destination chain.
+  payload: {
+    abi: any;
+    functionName: string;
+    args: any[];
+  }; // This will prepare bytecode for the action to execute on the destination chain.
   gasLimit: number // The maximum gas limit for the action on the destination chain.
 }
 
